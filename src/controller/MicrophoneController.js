0815e7499bc4867c70fbc05b5b116835cb076770
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2022-07-12 11:09:22 -0300

    Add: recording audio captured by microphone

--- src/controller/MicrophoneController.js
+++ src/controller/MicrophoneController.js
@@ -6,21 +6,27 @@ export class MicrophoneController extends ClassEvent{
 
         super();
 
+        this._mimeType = 'audio/webm';
+
+        this._available = false;
+
         navigator.mediaDevices.getUserMedia({ audio: true }).then(stream => {
+
+            this._available = true;
 
             this._stream = stream;
 
-            let audio = new Audio();
-
-            audio.src = URL.createObjectURL(stream);
-
-            audio.play();
-
-            this.trigger('play', audio);
+            this.trigger('ready', this._stream);
 
         }).catch(err => {
 
         });
+
+    }
+
+    isAvailable() {
+
+        return this._available;
 
     }
 
@@ -32,5 +38,61 @@ export class MicrophoneController extends ClassEvent{
 
     }
 
+    startRecorder() {
+
+        if(this.isAvailable()) {
+
+            this._mediaRecorder = new MediaRecorder(this._stream, {
+                mimeType: this._mimeType
+            });
+
+            this._recordedChunks = [];
+
+            this._mediaRecorder.addEventListener('dataavailable', e => {
+
+                if(e.data.size > 0) this._recordedChunks.push(e.data);
+
+            });
+
+            this._mediaRecorder.addEventListener('stop', e => {
+
+                let blob = new Blob(this._recordedChunks, {
+                    type: this._mimeType
+                });
+
+                let filename = `rec${Date.now()}.webm`;
+
+                let file = new File([blob], filename, {
+                    type: this._mimeType,
+                    lastModified: Date.now()
+                });
+
+                let reader = new FileReader();
+
+                reader.onload = e => {
+
+                    let audio = new Audio(reader.result);
+
+                    audio.play();
+
+                }
+
+            });
+
+            this._mediaRecorder.start();
+
+        }
+    }
+
+    stopRecorder() {
+
+        if(this.isAvailable()) {
+
+            this._mediaRecorder.stop();
+            this.stop();
+
+        }   
+
+    }
 
 }
